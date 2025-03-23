9b1e7a1dbbb7f3d1561d9fdaa7d39db841854435
thjomha <thjomha@gmail.com>
2025-03-23 10:49:20 -0600

    all-ips

--- app.py
+++ app.py
@@ -110,4 +110,5 @@ def home():
     return render_template("index.html", prayer_times=prayer_times)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
